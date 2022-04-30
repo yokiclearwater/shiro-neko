f799a0a5d87f10fc82fced17c47b48e3d40bb09f
YokiClearwater <yonkimkosal@gmail.com>
2022-04-30 13:00:06 +0700

    annoy

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,6 @@
 const { client } = require("../index");
 const {MessageAttachment} = require('discord.js')
-
+var myInterval;
 
 const slurs = ["nigga", "nigger"];
 
@@ -10,13 +10,6 @@ module.exports = {
 	async execute(message) {
 		if(message.author.bot) return;
 
-        // slurs.forEach(async (word) => {
-        //     if(message.content.toLowerCase().includes(word)) {
-        //         await timeout(2500)
-        //         message.reply("You're fucking racist");
-        //     }
-        // })
-
         const attachment = new MessageAttachment('https://media.discordapp.net/attachments/960927762691022949/969660968818864128/stsmall507x507-pad600x600f8f8f8.jpg');
         if(message.mentions.has(client.user)) {
             await timeout(1500);
@@ -24,6 +17,27 @@ module.exports = {
                 content: `${message.author}${message.author}${message.author}, Uno Reverse`,
                 files: [attachment]
             });
+        }
+
+        const clientInfo = await client.application.fetch();
+        if(message.content.startsWith("$annoy") && (message.author.id === clientInfo.owner.id)) {
+            const firstMention = message.mentions.users.first();
+            message.delete()
+            .then(msg => console.log(`Deleted message from ${msg.author.username}`))
+            .catch(console.error);
+            var messageFunction = () => {
+                if(message.mentions.users.first()) {
+                    message.channel.send(`<@${firstMention.id}>`).then(msg => msg.delete());
+                } else {
+                    message.channel.send('<@781254456855494676>').then(msg => msg.delete());
+                }
+            }
+            messageFunction()
+            myInterval = setInterval(messageFunction, 10000);
+        }
+
+        if(message.content === "$stop" && (message.author.id === clientInfo.owner.id)) {
+            clearInterval(myInterval);
         }
 	},
 };
